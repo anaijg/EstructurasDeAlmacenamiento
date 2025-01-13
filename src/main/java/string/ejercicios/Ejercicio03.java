e1b609d9f75a4c43a439e1588c00ab300c0b7b24
anaijg <ana.jimenezgomez@educa.madrid.org>
2025-01-13 19:22:21 +0100

    ejercicio 3

--- src/main/java/string/ejercicios/Ejercicio03.java
+++ src/main/java/string/ejercicios/Ejercicio03.java
@@ -1,11 +1,17 @@
 package string.ejercicios;
 
+import java.util.Scanner;
+
 /**
  * Ejercicio 3
  * Escribe un programa que lea una cadena de texto y reemplace todas las ocurrencias de la letra ‘a’ con la letra ‘b’. A continuación debe imprimir la cadena resultante.
  */
 public class Ejercicio03 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String cadena = scanner.nextLine();
+        // reemplazamos todas las ocurrencias de 'a' por 'b'
+        cadena = cadena.replace("a", "b");
+        System.out.println(cadena);
     }
 }
