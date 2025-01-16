4e415804f37fae76b3862468fdcd058e28621c06
anaijg <ana.jimenezgomez@educa.madrid.org>
2025-01-16 20:17:05 +0100

    teoría arrays 16/01

---
+++ src/main/java/array/EjemploNumerosAleatorios.java
@@ -0,0 +1,16 @@
+package array;
+
+import java.util.Random;
+
+public class EjemploArrayNumerosAleatorios {
+    public static void main(String[] args) {
+        // quiero un array de 30 números generados aleatoriamente entre 1 y 100
+
+        // cómo generar un número aleatorio
+        Random random = new Random(); // 1) creo un objeto random
+        for (int i = 0; i < 300; i++) {
+            int numero = random.nextInt(0, 101); // 2) llamo al método nextInt(min, max + 1);
+            System.out.print(numero + "\t");
+        }
+    }
+}
