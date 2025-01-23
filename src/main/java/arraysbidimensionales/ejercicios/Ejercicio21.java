d2794b7f109fa0b0dd40a6f0bb892ac75d19c6bc
anaijg <ana.jimenezgomez@educa.madrid.org>
2025-01-23 20:23:46 +0100

    Ejercicio 21

---
+++ src/main/java/arraysbidimensionales/ejercicios/Ejercicio21.java
@@ -0,0 +1,22 @@
+package arraysbidimensionales.ejercicios;
+
+/**
+ * Haz un programa que realice lo siguiente:
+ * Crea un array bidimensional de 5x5 enteros y nombre ‘diagonal’.
+ * Rellénalo de forma que los componentes pertenecientes a la diagonal de la matriz tomen el valor 1 y el resto el valor 0.
+ * Muestra el contenido del array en pantalla.
+ */
+public class Ejercicio21 {
+    public static void main(String[] args) {
+        int[][] diagonal = new int[5][5];
+        for (int i = 0; i < diagonal.length; i++) {
+            for (int j = 0; j < diagonal[i].length; j++) {
+                if(i == j) {
+                    diagonal[i][j] = 1;
+                }
+                System.out.print(diagonal[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
